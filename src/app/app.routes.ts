b934cb66968eb8245cff7e55f30edb94fe88a30f
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2026-01-13 13:02:48 +0300

    home, about, experience, contacts, projects route configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./features/projects/projects.component').then(
+        (m) => m.ProjectsComponent
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+  },
+  {
+    path: 'experience',
+    loadComponent: () =>
+      import('./features/experience/experience.component').then(
+        (m) => m.ExperienceComponent
+      ),
+  },
+  { path: '**', redirectTo: '' },
+];
